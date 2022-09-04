f41f5c579554f3b54977db622fd6050ef8452221
swolebrain <thecodingteacher@gmail.com>
2022-09-04 18:16:01 -0400

    change to h3 maybe?

--- appserver/src/server.ts
+++ appserver/src/server.ts
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8000;
 const ENV = process.env.ENV || 'staging';
 
 app.get("/", (req:Request, res:Response):void => {
-    res.send(`<h4>I didn't choose the thug life, the thug life chose me. This is ${ENV} env</h4>`);
+    res.send(`<h3>I didn't choose the thug life, the thug life chose me. This is ${ENV} env</h3>`);
 });
 
 app.listen(PORT, ():void => {
