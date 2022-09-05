015334360bdc0c6c386caeebfd554a81fb1de544
swolebrain <thecodingteacher@gmail.com>
2022-09-05 07:31:44 -0400

    improved doc for healthcheck endpoint

--- appserver/src/server.ts
+++ appserver/src/server.ts
@@ -3,8 +3,9 @@ const app:Application = express();
 const PORT = process.env.PORT || 8000;
 const ENV = process.env.ENV || 'staging';
 
+// Do not delete this endpoint or put it under auth. This is the health check.
 app.get("/", (req:Request, res:Response):void => {
-    res.send(`<h3>I didn't choose the thug life, the thug life chose me. This is ${ENV} env</h3>`);
+    res.send(`<h3>App is working. This is ${ENV} env</h3>`);
 });
 
 app.listen(PORT, ():void => {
